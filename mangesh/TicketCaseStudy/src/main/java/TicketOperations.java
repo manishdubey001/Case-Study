c76fd3d93a742207e7e9f3fe28ae40d418d8f9a3
mangeshsutar <mangesh.sutar@inin.com>
2016-01-19 15:41:16 +0530

    mangesh: new branch

--- mangesh/TicketCaseStudy/src/main/java/TicketOperations.java
+++ mangesh/TicketCaseStudy/src/main/java/TicketOperations.java
@@ -9,8 +9,11 @@ import java.util.*;
 public class TicketOperations {
 
     ArrayList<Ticket> masterTicketData = new ArrayList();
-    HashMap<String, Integer> agentTicketsTotalCounts = new HashMap<String, Integer>();
-    HashMap<String, HashSet> tagsWithTicketIds = new HashMap<String, HashSet>();
+    HashMap<String, Integer> agentTicketsTotalCounts = new HashMap<String, Integer>(); // Storing ticket counts against agents name
+    HashMap<String, HashSet> tagsWithTicketIds = new HashMap<String, HashSet>();  // storing ticket ids against tag name
+
+    TicketService ticketService = new TicketService();
+    InputData inputData = new InputData();
 
     Scanner console;
     public boolean loop;
@@ -21,8 +24,8 @@ public class TicketOperations {
     Date date = new Date();
     public long timeStamp = date.getTime();
 
-    public TicketOperations(){
-        ticketId = 100;
+    public TicketOperations(int id){
+        ticketId = id;
         console = new Scanner(System.in);
         br = new BufferedReader(new InputStreamReader(System.in));
         this.menuList();
@@ -81,9 +84,9 @@ public class TicketOperations {
                      break;
             case 2 : this.updateTicket();
                      break;
-            case 3 : this.deleteOrShowTicket(1);
+            case 3 : this.deleteTicketById();
                      break;
-            case 4 : this.deleteOrShowTicket(2);
+            case 4 : this.showTicketById();
                      break;
             case 5 : this.showAllTickets();
                      break;
@@ -91,7 +94,7 @@ public class TicketOperations {
                      break;
             case 7 : this.ticketCountsByAgentName();
                      break;
-            case 8 : this.searchTicketsByTagName();
+            case 8 : this.searchTicketsByTagName();  // In-progress
                      break;
             case 9 :
                 System.out.println("End of operation");
@@ -105,269 +108,102 @@ public class TicketOperations {
      * Adding tags
      */
     public void createTicket(){
-        boolean subjLoop = true;
-        System.out.println("Enter Ticket Subject : ");
-        String sub = null;
-        while (subjLoop){
-            sub = console.nextLine();
-            sub.trim();
-            if(!sub.isEmpty()){
-                subjLoop = false;
-            }
-        }
+        String subject = inputData.getString();
+        String agentName = inputData.getAgentName();
+        List<String> tagList = inputData.getTagsList();
 
-        boolean agentLoop = true;
-        String agent = null;
-        System.out.println("Enter Agent Name : ");
-        while (agentLoop){
-            agent = console.nextLine();
-            agent.trim();
-            if(!agent.isEmpty()){
-                agentLoop = false;
-                System.out.println("");
-            }
-        }
+        int returnValue = ticketService.createTicket(ticketId, subject, agentName, tagList);
 
-        System.out.println("Enter Multiple tags separated by comma(,) : ");
-        String tags = null;
-        try {
-            tags = br.readLine();
-
-
-        }catch (IOException io){
-
-        }
-
-        List<String> tagList = Arrays.asList(tags.split(","));
-
-        if (!tagList.isEmpty()) {
-            this.updateTags(tagList, ticketId);
-        }
-
-        Date date = new Date();
-
-        Ticket ticket = new Ticket();
-        ticket.setId(ticketId);
-        ticket.setSubject(sub);
-        ticket.setAgent_name(agent);
-        ticket.setTempTags(tags);
-        ticket.setTags(tagList);
-        ticket.setCreated(date);
-        ticket.setModified(date);
-
-        this.masterTicketData.add(ticket);
-        this.updateAgentsTicketCount(agent, 1);
         ticketId++;
 
-        System.out.println("Ticket has been added successfully");
+        if(returnValue == 1)
+            System.out.println("Ticket has been added successfully");
+        else
+            System.out.println("ERROR : Creating ticket false");
+
         this.menuList();
-    }
-
-    /**
-     * Updating Ticket count for a agent
-     * @param agent
-     * @param action
-     */
-    public void updateAgentsTicketCount(String agent, int action){
-        int agentCount = 0 ;
-        if(agentTicketsTotalCounts.containsKey(agent)){
-            agentCount = agentTicketsTotalCounts.get(agent);
-            if(action == 1) // add
-                agentCount = agentCount + 1;
-            else if(action == 0) // remove
-                agentCount = agentCount - 1;
-        }
-        else if(action == 1)
-            agentCount = 1;
-
-        agentTicketsTotalCounts.put(agent, agentCount);
     }
 
     /**
      * showing all ticket details
      */
     public void showAllTickets() {
-        // need to do sort on modified
-
-        Collections.sort(this.masterTicketData);
-
-        if(this.masterTicketData.isEmpty()){
-            System.out.println("No Tickets Found!!!");
-        }
-        else
-        {
-            System.out.println("Id | Subject | Agent Name | Tags | Created | Modified");
-            for (Ticket ticket : this.masterTicketData){
-                System.out.println(ticket.getId() + " | " + ticket.getSubject() + " | " + ticket.getAgent_name() + " | " + ticket.getTempTags() + " | " +
-                        ticket.getCreated() + " | " + ticket.getModified());
-            }
-            System.out.println("tags :: " + tagsWithTicketIds);
-            this.menuList();
-        }
+        ticketService.showAllTickets();
+        this.menuList();
     }
 
     /**
      * Update ticket details.
      */
     public void updateTicket(){
-        System.out.println("Enter Ticket Id :: ");
-        boolean updateLoop = true;
-        int id = 0;
-        while (updateLoop){
-            try {
-                id = console.nextInt();
-                if(this.isTicketExist(id)) {
-                    System.out.println("What do you want to update?");
-                    System.out.println("1. Agent Name");
-                    System.out.println("2. Tags");
-                    int updateTicketId;
-                    try {
-                        Date date = new Date();
-                        updateTicketId = Integer.valueOf(br.readLine());
-                        if(updateTicketId == 1) {
-                            for(Ticket ticket : masterTicketData) {
-                                if(ticket.getId() == id) {
-                                    System.out.println("Old Agent Name : " + ticket.getAgent_name());
-                                    System.out.println("Enter New Agent Name :: ");
-                                    String agentName = br.readLine();
-                                    ticket.setAgent_name(agentName);
-                                    ticket.setModified(date);
-                                    System.out.println("Agent Name is updated Successfully!!!");
-                                    this.menuList();
-                                }
-                            }
-                        }else if(updateTicketId == 2) {
-                            for(Ticket ticket : masterTicketData){
-                                if(ticket.getId() == id ){
-                                    System.out.println("Old tags :: " + ticket.getTempTags());
-                                    System.out.println("Enter New tags seperated by comma (,) :: ");
-                                    String newTags = br.readLine();
-                                   // if (!newTags.isEmpty()) {
+  // get ticket id from user
+        int ticketId = inputData.getTicketIdFromUser();
 
-                                        // update tags
-                                        List<String> newTagsList = Arrays.asList(newTags);
-                                        if(!newTagsList.isEmpty()){
-                                            this.updateTags(newTagsList, id);
-                                        }
+        if(ticketService.isTicketExist(ticketId)) {
+            //user choice
+            int userChoice = inputData.getUserChoiceForUpdate();
 
-                                        // remove
-                                        List<String> oldTicketTags = ticket.getTags();
-                                        this.removeTicketTags(oldTicketTags, id);
-
-                                        ticket.setTempTags(newTags);
-                                        ticket.setModified(date);
-                                        System.out.println("Ticket Tags are Updated Successfully!!!");
-
-
-
-                                        this.menuList();
-                                   // }
-                                }
-                            }
-
-                        }
-                    }
-                    catch(IOException ie){
-
-                    }
-                }
-                else
-                {
-                    System.out.println("Entered ticket " + id + " is not present in the system.");
-                    updateLoop = false;
-                    this.menuList();
-                }
-                updateLoop = false;
+            if(userChoice == 1){
+                String agentName = inputData.getAgentName();
+                ticketService.updateTicketAgent(ticketId, agentName);  // chk return value then print msg
+                System.out.println("Agent Name is updated Successfully!!!");
             }
-            catch (InputMismatchException ie){
-                System.out.println("Invalid Input !!! Please enter only Number :: ");
-                updateLoop = false;
-
+            else if(userChoice == 2){
+                List<String> tags = inputData.getTagsList();
+                ticketService.updateTicketTags(ticketId, tags); // chk return value then print msg
+                System.out.println("Ticket Tags are Updated Successfully!!!");
             }
+        }
+        else
+        {
+            System.out.println("Entered ticket " + ticketId + " is not present in the system.");
+            this.menuList();
         }
     }
 
-    /**
-     * check given ticket is present or not in the system.
-     * @param ticketId
-     * @return
-     */
-    public boolean isTicketExist(int ticketId) {
-        //System.out.println("ic check :: "  + masterTicketData.get(ticketId));
+    public void deleteTicketById(){
+        // get ticket id from user
+        int ticketId = inputData.getTicketIdFromUser();
 
-        for (Ticket ticket : masterTicketData){
-            if(ticket.getId() == ticketId)
-                return true;
+        if(ticketService.isTicketExist(ticketId)) {
+            if(ticketService.deleteTicket(ticketId)){
+                System.out.println("Ticket Has been deleted Successfully");
+            }
+            else {
+                System.out.println("ERROR - Ticket Not deleted");
+            }
         }
-        return false;
+        else
+        {
+            System.out.println("Entered ticket " + ticketId + " is not present in the system.");
+        }
+        this.menuList();
     }
 
-    /**
-     * Show ticket details by id & delete ticket by id
-     * @param choice
-     */
-    public void deleteOrShowTicket(int choice){
-        System.out.println("Enter Ticket Id :: ");
-        boolean loop = true;
-        int id = 0;
-        while (loop) {
-            try {
-                id = console.nextInt();
-                if(this.isTicketExist(id)) {
-                    for(Ticket ticket : masterTicketData) {
-                        if (ticket.getId() == id) {
+    public void showTicketById() {
+        // get ticket id from user
+        int ticketId = inputData.getTicketIdFromUser();
 
-                            if(choice == 1) { // delete
-                                masterTicketData.remove(ticket);
-                                System.out.println("Ticket Has been deleted Successfully");
-                                this.updateAgentsTicketCount(ticket.getAgent_name(), 0);
-                            }
-                            else if(choice == 2) { // display single ticket data
-                                System.out.println("Id | Subject | Agent Name | Tags | Created | Modified");
-                                System.out.println(ticket.getId() + " | " + ticket.getSubject() + " | " + ticket.getAgent_name() + " | " + ticket.getTempTags() + " | " +
-                                            ticket.getCreated() + " | " + ticket.getModified());
-                            }
-                            loop = false;
-                            this.menuList();
-                            break;
-                        }
-                    }
-                }
-                else{
-                    System.out.println("Entered ticket " + id + " is not present in the system.");
-                    loop = false;
-                    this.menuList();
-                }
-
-                loop = false;
-            }
-            catch (InputMismatchException ie){
-                System.out.println("Invalid Input !!! Please enter only Number :: ");
-                loop = false;
-            }
+        if(ticketService.isTicketExist(ticketId)) {
+            HashMap TicketDetails = ticketService.getTicketDetailsById(ticketId);
+            System.out.println("Id | Subject | Agent Name | Tags | Created | Modified");
+            System.out.println(TicketDetails.get("id") + " | " + TicketDetails.get("subject") + " | " + TicketDetails.get("agentName") + " | " + TicketDetails.get("tags") + " | " +
+                    TicketDetails.get("created") + " | " + TicketDetails.get("modified"));
         }
+        else
+        {
+            System.out.println("Entered ticket " + ticketId + " is not present in the system.");
+        }
+        this.menuList();
     }
 
     /**
      * Showing ticket by agent name
      */
     public void showTicketByAgentName() {
-        System.out.println("Enter Agent Name :: ");
-        String agentName = console.nextLine();
-        boolean isTicket = false;
-        agentName.trim();
-        System.out.println("Id | Subject | Agent Name | Tags | Created | Modified");
-        for (Ticket ticket : masterTicketData) {
-            if(ticket.getAgent_name().equals(agentName)){
-                System.out.println(ticket.getId() + " | " + ticket.getSubject() + " | " + ticket.getAgent_name() + " | " + ticket.getTempTags() + " | " +
-                        ticket.getCreated() + " | " + ticket.getModified());
-                isTicket = true;
-            }
-        }
-
-        if(isTicket == false) {
-            System.out.println("No Ticket(s) assigned to Agent " + agentName);
-        }
+        //user agent name
+        String agentName = inputData.getAgentName();
+        ticketService.showTicketByAgentName(agentName);
         this.menuList();
     }
 
@@ -375,16 +211,7 @@ public class TicketOperations {
      * Showing Ticket count(s) grouped by agent name
      */
     public void ticketCountsByAgentName(){
-        System.out.println("Agent Count  |   Total Count");
-
-        Set setOfKeys = agentTicketsTotalCounts.keySet();
-        Iterator it = setOfKeys.iterator();
-
-        while (it.hasNext()){
-            String agentName = (String) it.next();
-            Integer count = (Integer) agentTicketsTotalCounts.get(agentName);
-            System.out.println(agentName + "     |     " + count);
-        }
+        ticketService.ticketCountsByAgentName();
         this.menuList();
     }
 
@@ -392,65 +219,13 @@ public class TicketOperations {
      * Searching ticket by using tag name
      */
     public void searchTicketsByTagName() {
-        System.out.println("Enter Tag Name :: ");
-        String tagName = console.nextLine();
-        HashSet ids = new HashSet();
-        if(this.tagsWithTicketIds.containsKey(tagName)) {
-            ids = tagsWithTicketIds.get(tagName);
-        }
-
-        if(masterTicketData.isEmpty()){
-            System.out.println("No ticket(s) found in the system");
-        }
-        else{
-            System.out.println("Id | Subject | Agent Name | Tags | Created | Modified");
-            for (Ticket ticket : masterTicketData){
-                if(ids.contains(ticket.getId())){
-                    System.out.println(ticket.getId() + " | " + ticket.getSubject() + " | " + ticket.getAgent_name() + " | " + ticket.getTempTags() + " | " +
-                            ticket.getCreated() + " | " + ticket.getModified());
-                }
-            }
-            this.menuList();
-        }
-
+        String tag = inputData.getString();
+        ticketService.showTicketsByTagName(tag);
+        this.menuList();
     }
 
-    /**
-     * Adding / updating ticket ids into tag set
-     * @param tagsList
-     * @param ticketId
-     */
-    public void updateTags(List<String> tagsList, int ticketId){
-        for (String tag : tagsList) {
-            if (this.tagsWithTicketIds.containsKey(tag)) {
-                HashSet<Integer> idSet = new HashSet<Integer>();
-                idSet = tagsWithTicketIds.get(tag);
-                idSet.add(ticketId);
-                this.tagsWithTicketIds.put(tag, idSet);
-            } else {
-                HashSet<Integer> idSet = new HashSet<Integer>();
-                idSet.add(ticketId);
-                this.tagsWithTicketIds.put(tag, idSet);
-            }
-        }
-    }
-
-    /**
-     * Removing ticket id from tags set
-     * @param oldTicketTags
-     * @param ticketId
-     */
-    public void removeTicketTags(List<String> oldTicketTags, int ticketId){
-        for (String tag : oldTicketTags){
-            if(oldTicketTags.contains(tag)) {
-                HashSet allTags = this.tagsWithTicketIds.get(tag);
-                allTags.remove(ticketId);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         //TicketOperations ticketObj = new TicketOperations();
-        new TicketOperations();
+        new TicketOperations(100);
     }
 }
