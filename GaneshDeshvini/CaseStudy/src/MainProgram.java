af13bcd2cd61878c8df1bb99f80a1fea70b6814e
LokeshLodha <lokeshj3@gmail.com>
2016-01-19 13:45:54 +0530

    removed other code bases from lokesh branch

--- GaneshDeshvini/CaseStudy/src/MainProgram.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by root on 31/12/15.
- */
-public class MainProgram {
-    public static void main(String[] args) {
-        ConsoleMenu consoleMenu = new ConsoleMenu();
-        consoleMenu.start();
-    }
-}
